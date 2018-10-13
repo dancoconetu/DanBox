066d08e06b27d1b5ffa33399225c6aaebd3535f4
Dan <dancoconetu@gmail.com>
2018-10-13 19:12:45 +0200

    Onion Architecture

---
+++ BusinessLogicLayer/Interfaces/IQuestionRepository.cs
@@ -0,0 +1,12 @@
+﻿using BusinessLogicLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogicLayer.Interfaces
+{
+    public interface IQuestionRepository
+    {
+       IEnumerable<Question> getQuestions();
+    }
+}
